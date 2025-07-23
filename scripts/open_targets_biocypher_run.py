10e3bf3be42dde14b4de4dbd7f8151442f1841b7
Loes van den Biggelaar <loes@thehyve.nl>
2025-07-23 15:22:41 +0200

    Remove unused mouse model nodes

--- scripts/open_targets_biocypher_run.py
+++ scripts/open_targets_biocypher_run.py
@@ -13,8 +13,6 @@ from open_targets.definition import (
     node_diseases,
     node_gene_ontology,
     node_molecule,
-    node_mouse_phenotype,
-    node_mouse_target,
     node_targets,
 )
 
@@ -34,8 +32,6 @@ def main():
         node_diseases,
         node_molecule,
         node_gene_ontology,
-        node_mouse_phenotype,
-        node_mouse_target,
     ]
     edge_definitions = [edge_target_disease, edge_target_go, edge_drug_disease, edge_drug_target]
 
